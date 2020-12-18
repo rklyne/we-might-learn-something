58d4f485af0cb78f00cc4c5362edc93d88c5cc18
Ronan Klyne <ronan.klyne@cazoo.co.uk>
2020-12-18 16:05:09 +0000

    --wip-- [skip ci]

--- tdd2.test.ts
+++ tdd2.test.ts
@@ -48,43 +48,45 @@ describe("a calculator", () => {
   });
   describe("5 numbers", () => {
     it("should return 15 for '1,2,3,4,5'", () => {
-      expect(add('1,2,3,4,5')).toBe(15);
-    })
-  })
+      expect(add("1,2,3,4,5")).toBe(15);
+    });
+  });
   describe("5000 numbers", () => {
     it("should return 5000 for '1,1,1,1,....'", () => {
-      expect(add('1,'.repeat(4999)+'1')).toBe(5000);
-    })
-  })
+      expect(add("1,".repeat(4999) + "1")).toBe(5000);
+    });
+  });
   describe("newline separated numbers", () => {
     it("should return 6 for '1\n2,3'", () => {
-      expect(add('1\n2,3')).toBe(6);
-    })
+      expect(add("1\n2,3")).toBe(6);
+    });
     it("should return 1 for '1,\n'", () => {
-      expect(add('1,\n')).toBe(1);
-    })
+      expect(add("1,\n")).toBe(1);
+    });
     it("should return 20 for '1\n6\n10\n3'", () => {
-        expect(add('1\n6\n10\n3')).toBe(20);
-    })
-  })
+      expect(add("1\n6\n10\n3")).toBe(20);
+    });
+  });
   describe("custom delimiters", () => {
     it("should return 6 for '//;\n1;2\n3'", () => {
-      expect(add('//;\n1;2\n3')).toBe(6);
-    })
-    it("should return 8 for '//;\n1;2\n5'", () => {
-        expect(add('//;?@\n1;?@2\n5')).toBe(8);
-    })
-  })
+      expect(add("//;\n1;2\n3")).toBe(6);
+    });
+    it("should return 8 for '//;?@\n1;?@2\n5'", () => {
+      expect(add("//;?@\n1;?@2\n5")).toBe(8);
+    });
+  });
 });
 
 function add(text: string): number {
-    let delimiter = ',';
-    if(text.indexOf('//') > -1) {
-        delimiter = text[2];
-        text = text.substr(4);
-    }
-    return text
+  let delimiter = ",";
+  let innertext = text;
+  if (text.indexOf("//") > -1) {
+    const delimiterEnd = text.indexOf("\n");
+    delimiter = text.substr(0, delimiterEnd).substr(2);
+    innertext = text.substr(delimiterEnd + 1);
+  }
+  return innertext
     .replace(/\n/g, delimiter)
     .split(delimiter)
-    .reduce((a: number, b: string) => a + parseFloat(b || '0'), 0);
+    .reduce((a: number, b: string) => a + parseFloat(b || "0"), 0);
 }
