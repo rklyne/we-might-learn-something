05e49af1fbfe9b081efc40e6eeed53977a812c3d
Ronan Klyne <ronan.klyne@cazoo.co.uk>
2020-12-18 15:08:21 +0000

    --wip-- [skip ci]

--- tdd2.test.ts
+++ tdd2.test.ts
@@ -1,5 +1,3 @@
-
-
 /**
  * Step 1
  * Create a simple String calculator with a method signature:
@@ -9,45 +7,39 @@
  * For an empty string it will return 0.
  */
 
-
 describe("a calculator", () => {
   it("should return zero for the empty string", () => {
     expect(add("")).toBe(0);
-  })
+  });
   describe("single numbers", () => {
     it("should return 1 given 1", () => {
       expect(add("1")).toBe(1);
-    })
+    });
     it("should return 2 given 2", () => {
       expect(add("2")).toBe(2);
-    })
+    });
     it("should return 10 given 10", () => {
       expect(add("10")).toBe(10);
-    })
-  })
+    });
+  });
   describe("two numbers", () => {
     it("should return 2 given 1,1", () => {
       expect(add("1,1")).toBe(2);
-    })
+    });
     it("should return 3 given 1,2", () => {
       expect(add("1,2")).toBe(3);
-    })
+    });
     it("should return 4 given 2,2", () => {
       expect(add("2,2")).toBe(4);
-    })
-  })
-})
-
+    });
+  });
+});
 
 function add(text: string): number {
   if (!text) {
     return 0;
   }
-  if (text.indexOf(',') != -1) {
-    if (text.indexOf('2') != -1) {
-        return 3;
-    }
-    return 2;
-  }
-  return parseInt(text);
+  return text
+    .split(",")
+    .reduce((a: number, b: string) => a + parseInt(b), 0);
 }
