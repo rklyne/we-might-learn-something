9ecfff95d01998194cbfedc29a6f37416b18ada6
Ronan Klyne <ronan.klyne@cazoo.co.uk>
2020-12-18 14:37:39 +0000

    🔴🔴🔴🔴 RED -  - to squash

---
+++ tdd2.test.ts
@@ -0,0 +1,21 @@
+
+
+/**
+ * Step 1
+ * Create a simple String calculator with a method signature:
+ *     int functionname(string numbers)
+ * The method can take up to two numbers, separated by commas, and will return their sum.
+ * For example "" or "1" or "1,2" as inputs.
+ * For an empty string it will return 0.
+ */
+
+
+describe("a calculator", () => {
+  it("should return zero for the empty string", () => {
+    expect(add("")).toBe(0);
+  })
+})
+
+
+function add(text: string): number {
+}
